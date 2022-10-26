aa6e4ec2d5f1d8fb41b03c96cefbc4326ecc55a2
Cheol Lee <chullee49@naver.com>
2022-10-26 19:31:50 +0900

    Lock Aop

--- src/main/java/com/example/account/dto/DeleteAccount.java
+++ src/main/java/com/example/account/dto/DeleteAccount.java
@@ -3,21 +3,23 @@ package com.example.account.dto;
 import lombok.*;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
-public class CreateAccount {
+public class DeleteAccount {
     @Getter
     @Setter
     @AllArgsConstructor
-    public static class Request{
+    public static class Request {
         @NotNull
         @Min(1)
         private Long userId;
 
-        @NotNull
-        @Min(0)
-        private Long initialBalance;
+        @NotBlank
+        @Size(min = 10, max = 10)
+        private String accountNumber;
     }
 
     @Getter
@@ -25,16 +27,16 @@ public class CreateAccount {
     @NoArgsConstructor
     @AllArgsConstructor
     @Builder
-    public static class Response{
+    public static class Response {
         private Long userId;
         private String accountNumber;
-        private LocalDateTime registeredAt;
+        private LocalDateTime unRegisteredAt;
 
         public static Response from(AccountDto accountDto) {
             return Response.builder()
                     .userId(accountDto.getUserId())
                     .accountNumber(accountDto.getAccountNumber())
-                    .registeredAt(accountDto.getRegisteredAt())
+                    .unRegisteredAt(accountDto.getUnRegisteredAt())
                     .build();
         }
     }
